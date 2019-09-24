12bf68a5ec80da2029fa20f01497e3a336b076f6
Taras48 <red.shuhov48@gmail.com>
2019-09-24 19:05:02 +0300

    Merge branch 'master' of D:\TARAZ\Java\Personal\PersonalOne with conflicts.

--- src/main/java/all/dao/UserDaoFactory.java
+++ src/main/java/all/dao/UserDaoFactory.java
@@ -6,24 +6,19 @@ import all.dbHelper.DBHelper;
 import java.io.*;
 import java.util.Scanner;
 
-public  class UserDaoFactory {
-
-    private static String getProperties() {
+public class UserDaoFactory {
+    private String getProperties() {
         String str = "";
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        InputStream fileReader = classLoader.getResourceAsStream("db.properties");
-        int i = -1;
-            try {
-                while ((i = fileReader.read()) != -1) {
-                    str += (char) i;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        try (BufferedReader bufferedReader = new BufferedReader
+               (new FileReader("D:\\TARAZ\\Java\\Personal\\PersonalOne\\src\\main\\java\\all\\resources\\db.properties.txt"))) {
+            str = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return str;
     }
 
-    public UserDao getUserDao() {
+    public UserDao create() {
         if (getProperties().contains("sql")) {
             return new UserDaoJDBCimpl(DBHelper.getInstance().getConnection());
         } else {
